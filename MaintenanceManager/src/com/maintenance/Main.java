5f49a600d06a779cb26e261bcff8ee3d527b5501
thaleradm <thaleradm@ilzwinihprod015.sgi2.root.magnapowertrain.com>
2018-03-14 10:01:17 +0100

    ...

--- MaintenanceManager/src/com/maintenance/Main.java
+++ MaintenanceManager/src/com/maintenance/Main.java
@@ -76,6 +76,7 @@ public class Main extends Application {
 
 	// Java Entwicklungsversion
 	public final static String JDK = "1.8.0_152";
+	
 
 	private static final Logger logger = Logger.getLogger(Main.class);
 	private ResourceBundle resources = ResourceBundle.getBundle("language");
