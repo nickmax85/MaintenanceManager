5c133850cceff6080f76f4af0c026bad9c278afa
nickmax85 <nickmax85@gmx.at>
2019-01-18 09:16:27 +0100

    ..........

--- MaintenanceManager/src/com/maintenance/mail/MaintenanceMailNotifier.java
+++ MaintenanceManager/src/com/maintenance/mail/MaintenanceMailNotifier.java
@@ -230,7 +230,7 @@ public class MaintenanceMailNotifier extends Application {
 
 		text += "Wartung ist fällig am : " + df.format(nextWartungDate);
 		text += "\n\n";
-		text += "Software für Wartungsrückmeldung: " + "http://10.176.45.4/software/TPMTool.jar";
+		text += "Software für Wartungsrückmeldung: " + "http://10.176.45.4/software/TPMMaintenance.exe";
 		text += "\n\n";
 		text += "Diese Nachricht wurde an folgende Adressen versendet: " + to.replaceAll(",", "; ");
 		text += "\n\n\n\n\n";
